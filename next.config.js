c40289d9d5887d450120a30af3d370df12a657b6
Ian Luddy <iluddy@zendesk.com>
2022-10-07 11:33:57 +0100

    Config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withExportImages = require('next-export-optimize-images')
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
-  }
+  },
 }
 
-module.exports = nextConfig
+
+module.exports = withExportImages(nextConfig)
