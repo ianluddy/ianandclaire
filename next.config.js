34d9744dcc0e0df90194acc085109c6dbf9d4643
ian luddy <ianluddy@gmail.com>
2022-10-17 21:23:29 +0100

    Performance fixes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
-// const withExportImages = require('next-export-optimize-images')
 const nextConfig = {
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
 }
-
-
-// module.exports = withExportImages(nextConfig)
 module.exports = nextConfig;
