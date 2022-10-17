6d1cb7b96ac0cf9aab4a268fd39ea56782dbbab5
ian luddy <ianluddy@gmail.com>
2022-10-17 21:04:40 +0100

    Performance fixes

--- pages/_document.js
+++ pages/_document.js
@@ -18,9 +18,6 @@ export default class MyDocument extends Document {
         ...initialProps,
         styles: (
           <>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-            <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Abhaya+Libre&family=Cormorant&display=swap" rel="stylesheet"/>
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
