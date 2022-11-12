5ffc55760c968a3378aa452d10aa8426ede86f86
Ian Luddy <iluddy@zendesk.com>
2022-11-12 09:54:31 +0000

    Revert "100vh hero"
    This reverts commit 9c4c4b15f1ef75f887eaa05cac675a8c10871f8a.

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,13 @@ import Page from './page';
 
 const Layout = styled.div`
   max-width: 600px;
-  margin: 0 auto;
+  margin: 2.5rem auto 0 auto;
+  @media (${theme.devices.xs}) {
+    margin: 3rem auto 0 auto;
+  }
+  @media (${theme.devices.sm}) {
+    margin: 4rem auto 0 auto;
+  }
   @media (${theme.devices.md}) {
     margin: 5rem auto 0 auto;
   }
