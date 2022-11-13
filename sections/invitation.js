ac79e63530d75457e8e61b3d3fef43c96dd77ce8
Ian Luddy <iluddy@zendesk.com>
2022-11-13 10:32:21 +0000

    Invite

--- sections/invitation.js
+++ sections/invitation.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import Image from '../components/image';
 import Menu from '../components/menu';
-import Paragraph from '../components/paragraph';
+import BaseWrapper from '../components/wrapper';
+import Quote from '../components/quote';
 import { AnchorButton } from '../components/button';
 
 const Wrapper = styled.div`
@@ -11,64 +12,74 @@ const Wrapper = styled.div`
   padding: 0 1rem;
   display: flex;
   flex-direction: column;
+  text-transform: uppercase;
   @media (${theme.devices.md}) {
     display: block;
     padding: 0;
   }
-
-  p {
-    font-size: 1.2em;
-    text-transform: uppercase;
-    line-height: 30px;
-  }
-`;
-
-const ParagraphWrapper = styled.div`
-  order: 3;
-  @media (${theme.devices.md}) {
-    order: unset;
-  }
 `;
 
 const ButtonWrapper = styled.div`
-  order: 4;
-  margin: 2rem 0;
-  @media (${theme.devices.md}) {
-    order: unset;
-  }
 `;
 
 const ImageWrapper = styled.div`
-  order: 2;
-  margin: 1rem;
+  margin: 2rem auto 1rem auto;
+  width: 450px;
+`;
+
+const Title = styled(BaseWrapper)`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  img {
+    width: 20px !important;
+    @media (${theme.devices.xs}) {
+      width: 28px !important;
+    }
+    @media (${theme.devices.sm}) {
+      width: 35px !important;
+    }
+    @media (${theme.devices.md}) {
+      width: 40px !important;
+    }
+  }
   @media (${theme.devices.md}) {
-    order: unset;
-    margin: 2rem auto 2rem auto;
-    width: 370px;
+    max-width: 450px;
   }
 `;
 
-const Title = styled.div`
-  order: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+const Paragraph = styled.div`
+  text-align: center;
+  letter-spacing: 0.02em;
+  font-weight: 500;
+  margin: ${(props) => (props.margin || '1rem auto')};
+  letter-spacing: 0.02em;
+  font-size: 1.2em;
+  line-height: 1.6em;
+  @media (${theme.devices.xs}) {
+    font-size: 1.2em;
+  }
+  @media (${theme.devices.sm}) {
+    font-size: 1.2em;
+  }
   @media (${theme.devices.md}) {
-    flex-direction: row;
+    font-weight: 400;
+    font-size: 1.15em;
+    max-width: 450px;
+    letter-spacing: 0.025em;
   }
 `;
 
 export default function ({}) {
   return (
     <Wrapper>
-      <Paragraph> Together with their families </Paragraph>
+      <Paragraph> With great pleasure </Paragraph>
       <Title>
         <Image src="/images/svgs/flowers.svg" alt="Flowers" title="Flowers" width="40px" priority="true" hideMobile="true"/>
         <h1> Ian & Claire </h1>
         <Image src="/images/svgs/flowers.svg" alt="Flowers" title="Flowers" width="40px" priority="true" transform="scaleX(-1)" hideMobile="true"/>
       </Title>
-      <Paragraph> Invite you to join them <br/> at their Wedding </Paragraph>
+      <Paragraph> Invite you to join them <br/> on their Wedding day </Paragraph>
       <ImageWrapper>
         <Image src="/images/hall.jpg" alt="City Hall" title="City Hall" priority="true"/>
       </ImageWrapper>
